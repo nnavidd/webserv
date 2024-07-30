2aaa07312fbdb49a0050ce3155e64982d01a677d
nnavidd <navid.nabaeei@gmail.com>
2024-07-30 14:59:14 +0200

    fix: work on GetHandler class.

--- src/request/GetHandler.cpp
+++ src/request/GetHandler.cpp
@@ -3,57 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   GetHandler.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnabaeei <nnabaeei@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: nnavidd <nnavidd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 22:41:26 by nnabaeei          #+#    #+#             */
-/*   Updated: 2024/07/29 22:47:28 by nnabaeei         ###   ########.fr       */
+/*   Updated: 2024/07/30 14:50:02 by nnavidd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./GetHandler.hpp"
+#include "GetHandler.hpp"
 
-GetHandler::GetHandler() {}
+GetHandler::GetHandler(const std::map<std::string, std::string>& requestMap,
+                       const std::map<std::string, std::string>& serverConfig) :
+    HTTPResponse(serverConfig) {
+    setResponseRequestMap(requestMap);
+}
 
 GetHandler::~GetHandler() {}
 
-std::string GethHandler::GetMethod() {
-    std::ostringstream reponseHeaders;
-    std::string date, lastMfd, eTag;
+std::string GetHandler::GetMethod() {
+    std::ostringstream responseHeaders;
+    std::string date, lastMdf, eTag;
 
     if (_requestMap["uri"] == "/" || _requestMap["uri"] == "/index.html") {
         std::string readHtml = readHtmlFile("./src/index.html");
-        eTag = generateETag("./src/index.html", date, lastMfd);
+        eTag = generateETag("./src/index.html", date, lastMdf);
 
-        reponseHeaders << httpStatusCode(200)
-                       << "Server: " << _serverConfig["server_name"] << CRLF
-                       << "Date: " << date << CRLF
-                       << "Content-Type: text/html" << CRLF
-                       << "Content-Length: " << readHtml.size() << CRLF
-                       << "Last-Modified: " << lastMfd << CRLF
-                       << "Connection: close" << CRLF
-                       << "ETag: " << eTag << CRLF
-                       << "Cache-Control: no-cache" << CRLF
-                       << "Accept-Ranges: bytes" << CRLF << CRLF
-                       << readHtml;
-        return reponseHeaders.str();
+        responseHeaders << httpStatusCode(200)
+                        << "Server: " << _serverConfig.at("server_name") << CRLF
+                        << "Date: " << date << CRLF
+                        << "Content-Type: text/html" << CRLF
+                        << "Content-Length: " << readHtml.size() << CRLF
+                        << "Last-Modified: " << lastMdf << CRLF
+                        << "Connection: close" << CRLF
+                        << "ETag: " << eTag << CRLF
+                        << "Cache-Control: no-cache" << CRLF
+                        << "Accept-Ranges: bytes" << CRLF << CRLF
+                        << readHtml;
+        return responseHeaders.str();
     } else if (_requestMap["uri"] == "/favicon.ico") {
         std::string favicon = readBinaryFile("./src/request/favicon.ico");
-        eTag = generateETag("./src/request/favicon.ico", date, lastMfd);
+        eTag = generateETag("./src/request/favicon.ico", date, lastMdf);
 
-        reponseHeaders << httpStatusCode(200)
-                       << "Server: " << _serverConfig["server_name"] << CRLF
-                       << "Date: " << date << CRLF
-                       << "Content-Type: image/vnd.microsoft.icon" << CRLF
-                       << "Content-Length: " << favicon.size() << CRLF
-                       << "Last-Modified: " << lastMfd << CRLF
-                       << "Connection: close" << CRLF
-                       << "ETag: " << eTag << CRLF
-                       << "Cache-Control: no-cache" << CRLF
-                       << "Accept-Ranges: bytes" << CRLF << CRLF;
-        reponseHeaders << favicon;
-        return reponseHeaders.str();
+        responseHeaders << httpStatusCode(200)
+                        << "Server: " << _serverConfig.at("server_name") << CRLF
+                        << "Date: " << date << CRLF
+                        << "Content-Type: image/vnd.microsoft.icon" << CRLF
+                        << "Content-Length: " << favicon.size() << CRLF
+                        << "Last-Modified: " << lastMdf << CRLF
+                        << "Connection: close" << CRLF
+                        << "ETag: " << eTag << CRLF
+                        << "Cache-Control: no-cache" << CRLF
+                        << "Accept-Ranges: bytes" << CRLF << CRLF;
+        responseHeaders << favicon;
+        return responseHeaders.str();
     } else {
         return httpStatusCode(404) + "Content-Type: text/html\r\n\r\n"
                "<html><head><title>404 Not Found</title></head><body><center><h1>404 Not Found</h1></body></html>";
     }
 }
+
+// std::string GetHandler::createHandleGet() {
+//     return GetMethod();
+// }
--- src/request/GetHandler.hpp
+++ src/request/GetHandler.hpp
@@ -3,24 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   GetHandler.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnabaeei <nnabaeei@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: nnavidd <nnavidd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 22:36:51 by nnabaeei          #+#    #+#             */
-/*   Updated: 2024/07/29 22:47:55 by nnabaeei         ###   ########.fr       */
+/*   Updated: 2024/07/30 14:49:54 by nnavidd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GETHANDLER_HPP
-# define  GETHANDLER_HPP
+# define GETHANDLER_HPP
 
-# include "./HttpResponse.hpp"
+# include "HttpResponse.hpp"
 
-class GetHandler: public HTTPResponse {
-	private:
+class GetHandler : public HTTPResponse {
 	public:
-		GetHandler();
+		GetHandler(const std::map<std::string, std::string>& requestMap, const std::map<std::string, std::string>& serverConfig);
 		~GetHandler();
-		GetMethod();
+		std::string GetMethod();
+	
+	// protected:
+	// 	std::string createHandleGet();
 };
 
-#endif
+#endif // GETHANDLER_HPP
