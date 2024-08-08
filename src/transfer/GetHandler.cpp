bb5f88e983f2f6f41d1c724cce221124a8854243
nnavidd <navid.nabaeei@gmail.com>
2024-08-08 17:05:22 +0200

    fix: Optimize: GetMethod(), cgi(), createEnv().
    Make Shorter, And Clear The GetMethod()
    Fix The Warring Come From The cgi(), Handle Post Is Added too.
    Optimize createEnv() To Be Possilbe To Add More Env According To The Server Config.

--- src/transfer/GetHandler.cpp
+++ src/transfer/GetHandler.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   GetHandler.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fahmadia <fahmadia@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: nnabaeei <nnabaeei@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 22:41:26 by nnabaeei          #+#    #+#             */
-/*   Updated: 2024/08/07 10:48:21 by fahmadia         ###   ########.fr       */
+/*   Updated: 2024/08/08 16:50:25 by nnabaeei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,19 @@ std::string GetHandler::GetMethod()
 	std::ostringstream responseHeaders;
 	std::string date, lastMfd, eTag;
 	std::string uri = _requestMap["uri"];
-	// std::cout << ":::::::: ----- " << uri << std::endl;
 	
 	if (uri == "/" || uri == "/index.html") 
 		uri = "/index.html";
 	
-	std::string filePath = _serverConfig.at("root") + uri;			//./www/serve + uri
+	std::string filePath = _serverConfig.at("root") + uri;
 	std::cout << ":::::::: ----- " << filePath << std::endl;
-	std::string readHtml = readHtmlFile(filePath);
+	std::string readHtml = readBinaryFile(filePath);
 
 	if (!readHtml.empty())
 	{
 		std::string extension = filePath.substr(filePath.find_last_of("."));
+		if (extension == CGI)
+			readHtml = cgi(uri);
 		std::string mimeType = getMimeType(extension);
 		eTag = generateETag(filePath, date, lastMfd);
 
@@ -54,49 +55,50 @@ std::string GetHandler::GetMethod()
 		}
 		responseHeaders << readHtml;
 		return responseHeaders.str();
-	}
-	else if (uri == "/cgi-bin/myScript.sh") // need to be dynamical
-	{
-		// std::string filePath = _serverConfig.at("root") + uri;
-		std::string extension = filePath.substr(filePath.find_last_of("."));
-		std::string mimeType = getMimeType(extension);
-		std::string cgiResult = cgi(uri); // ---- nico
-		// if (cgiResult.empty()) { // an error occurred
+	// }
+	// else if (uri == "/cgi-bin/myScript.sh") // need to be dynamical
+	// {
+	// 	// std::string filePath = _serverConfig.at("root") + uri;
+	// 	std::string extension = filePath.substr(filePath.find_last_of("."));
+	// 	std::string mimeType = getMimeType(extension);
+	// 	std::string cgiResult = cgi(uri); // ---- nico
+	// 	// if (cgiResult.empty()) { // an error occurred
 
-		// }
-		eTag = generateETag(uri, date, lastMfd);
+	// 	// }
+	// 	eTag = generateETag(uri, date, lastMfd);
 
-		responseHeaders	<< httpStatusCode(200)
-						<< "Date: " 					<< date << CRLF
-						<< "Server: " 					<< _serverConfig["server_name"] << CRLF
-						<< "Last-Modified: " 			<< lastMfd << CRLF
-						<< "ETag: " 					<< eTag << CRLF
-						<< "Content-Length: " 			<< cgiResult.size() << CRLF
-						<< "Cache-Control: no-cache" 	<< CRLF
-						<< "Content-Type: "				<< mimeType 		<< CRLF
-						<< "Connection: keep-alive" 	<< CRLF << CRLF;
-		responseHeaders << cgiResult;
-		return responseHeaders.str();
-	}
-	else if (uri == "/favicon.ico")
-	{
-		std::string extension = filePath.substr(filePath.find_last_of("."));
-		std::string mimeType = getMimeType(extension);
-		std::string favIconSize = readHtmlFile(filePath);
-		std::string favicon = readBinaryFile(filePath);
-		eTag = generateETag(uri, date, lastMfd);
+	// 	responseHeaders	<< httpStatusCode(200)
+	// 					<< "Date: " 					<< date << CRLF
+	// 					<< "Server: " 					<< _serverConfig["server_name"] << CRLF
+	// 					<< "Last-Modified: " 			<< lastMfd << CRLF
+	// 					<< "ETag: " 					<< eTag << CRLF
+	// 					<< "Content-Length: " 			<< cgiResult.size() << CRLF
+	// 					<< "Cache-Control: no-cache" 	<< CRLF
+	// 					<< "Content-Type: "				<< mimeType 		<< CRLF
+	// 					<< "Connection: keep-alive" 	<< CRLF << CRLF;
+	// 	responseHeaders << cgiResult;
+	// 	return responseHeaders.str();
+	// }
+	// else if (uri == "/favicon.ico")
+	// {
+	// 	std::string extension = filePath.substr(filePath.find_last_of("."));
+	// 	std::string mimeType = getMimeType(extension);
+	// 	std::string favIconSize = readHtmlFile(filePath);
+	// 	std::string favIcon = readBinaryFile(filePath);
+	// 	std::cout << MAGENTA "size: "<< favIcon.size() << "binary size: "<< favIconSize.size() << std::endl;
+	// 	eTag = generateETag(uri, date, lastMfd);
 
-		responseHeaders	<< httpStatusCode(200) 
-						<< "Date: " 					<< date << CRLF
-						<< "Server: " 					<< _serverConfig.at("server_name") << CRLF
-						<< "Last-Modified: " 			<< lastMfd << CRLF
-						<< "ETag: " 					<< eTag << CRLF
-						<< "Content-Length: " 			<< favIconSize.size() << CRLF
-						<< "Cache-Control: no-cache" 	<< CRLF
-						<< "Content-Type: "				<< mimeType 		<< CRLF
-						<< "Connection: keep-alive" 	<< CRLF << CRLF;
-		responseHeaders	<< favicon;
-		return responseHeaders.str();
+	// 	responseHeaders	<< httpStatusCode(200) 
+	// 					<< "Date: " 					<< date << CRLF
+	// 					<< "Server: " 					<< _serverConfig.at("server_name") << CRLF
+	// 					<< "Last-Modified: " 			<< lastMfd << CRLF
+	// 					<< "ETag: " 					<< eTag << CRLF
+	// 					<< "Content-Length: " 			<< favIcon.size() << CRLF
+	// 					<< "Cache-Control: no-cache" 	<< CRLF
+	// 					<< "Content-Type: "				<< mimeType 		<< CRLF
+	// 					<< "Connection: keep-alive" 	<< CRLF << CRLF;
+	// 	responseHeaders	<< favIcon;
+	// 	return responseHeaders.str();
 	} else
 		return httpStatusCode(404) + "Content-Type: text/html\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>";
 }
